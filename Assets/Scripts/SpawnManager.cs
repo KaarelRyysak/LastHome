58a63e6339e2806bc00a358b763841c0d06a844d
KaarelRyysak <kaarelgames@ryysak.com>
2019-01-26 21:42:51 +0200

    minor fixes

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -36,7 +36,7 @@ public class SpawnManager : MonoBehaviour {
                 waves[waveIndex].Count -= 1;
                 if(waves[waveIndex].Count >= 0)
                 {
-                    makeHuman();
+                    MakeHuman();
                 }
                 else
                 {
@@ -52,7 +52,7 @@ public class SpawnManager : MonoBehaviour {
         }
     }
     
-    private void makeHuman()
+    private void MakeHuman()
     {
         Human human = Instantiate(humanPrefab, gameObject.transform.position, gameObject.transform.rotation);
         numOfAlive += 1;
@@ -60,13 +60,13 @@ public class SpawnManager : MonoBehaviour {
         human.male = male;
         if (male)
         {
-            human.GetComponent<SpriteRenderer>().sprite = maleHeads[Mathf.RoundToInt(Random.Range(-0.49f, (float)maleHeads.Capacity - 0.51f))];
+            human.GetComponent<SpriteRenderer>().sprite = maleHeads[Mathf.RoundToInt(Random.Range(-0.49f, maleHeads.Capacity - 0.51f))];
         }
         else
         {
-            human.GetComponent<SpriteRenderer>().sprite = femaleHeads[Mathf.RoundToInt(Random.Range(-0.49f, (float)femaleHeads.Capacity - 0.51f))];
+            human.GetComponent<SpriteRenderer>().sprite = femaleHeads[Mathf.RoundToInt(Random.Range(-0.49f, femaleHeads.Capacity - 0.51f))];
         }
-        human.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = bodies[Mathf.RoundToInt(Random.Range(-0.49f, (float)bodies.Capacity - 0.51f))];
+        human.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = bodies[Mathf.RoundToInt(Random.Range(-0.49f, bodies.Capacity - 0.51f))];
     }
 
 }
