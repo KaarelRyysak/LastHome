634cd4b5fc5b105564fba7a352e02027856a2c3e
TornOne <ott.adermann@gmail.com>
2019-01-26 21:32:51 +0200

    Big update

---
+++ Assets/Scripts/Humans/SpawnManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+	public static SpawnManager instance;
+	public Human humanPrefab;
+	public List<Wave> waves;
+	private float lastSpawn;
+	public int numOfAlive;
+	private int waveIndex;
+	private bool waveSpawned;
+
+	private void Awake() {
+		instance = this;
+
+		lastSpawn = Time.time;
+
+		waveIndex = 0;
+
+		numOfAlive = 0;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (!waveSpawned) {
+			if (waveIndex < waves.Count && Time.time > lastSpawn + waves[waveIndex].Cooldown) {
+				lastSpawn = Time.time;
+				waves[waveIndex].Count -= 1;
+				if (waves[waveIndex].Count >= 0) {
+					Instantiate(humanPrefab, gameObject.transform.position, gameObject.transform.rotation);
+					numOfAlive += 1;
+				} else {
+					waveSpawned = true;
+				}
+			}
+		} else if (waveIndex < waves.Count && numOfAlive == 0) {
+			//Start new wave
+			waveIndex ++;
+			waveSpawned = false;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Traps/RotatingBlades.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotatingBlades : MonoBehaviour {
+	public float rotationAmount = 150f;
+	private Trap trap;
+	
+	void Start() {
+		trap = gameObject.GetComponent<Trap>();
+	}
+
+	void Update() {
+		if (trap.activated) {
+			transform.Rotate(new Vector3(0, 0, -rotationAmount * 10 * Time.deltaTime));
+		} else {
+			transform.Rotate(new Vector3(0, 0, -rotationAmount * Time.deltaTime));
+		}
+	}
+}
