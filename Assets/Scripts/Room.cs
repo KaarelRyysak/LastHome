c2fef1329391793485bcc82ab9e617596167745d
TornOne <ott.adermann@gmail.com>
2019-01-26 10:41:22 +0200

    Update Room.cs

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Room : MonoBehaviour {
 	public int y, x;
+
+	public List<Room> neighbors = new List<Room>();
 }
