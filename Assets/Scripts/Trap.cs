92fe9c3d953fed15bd9ef7f1d07752b9d58e8970
KaarelRyysak <kaarelgames@ryysak.com>
2019-01-26 10:03:22 +0200

    Added basic TrapDoor

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public bool activated;
+    public Sprite activatedSprite;
+    public Sprite disabledSprite;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+
+        //Inactive on startup
+        activated = false;
+        spriteRenderer.sprite = disabledSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //Toggle active on click
+        activated = !activated;
+
+        //Change sprite
+        if (activated)
+        {
+            spriteRenderer.sprite = activatedSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = disabledSprite;
+        }
+    }
+
+}
