68c9e6406b792a0d8d8bcd2df799fe0233c3abb4
KaarelRyysak <kaarelgames@ryysak.com>
2019-01-26 13:23:45 +0200

    Camera update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Panning")]
     public float upperBound = 10f;
     public float lowerBound = -10f;
     public float leftBound = -15f;
     public float rightBound = 15f;
+    public float panSpeed = 1f;
 
+
+    [Header("This is a percentage of the whole screen")]
+    public float panEdgeSize = 0.15f;
+
+    [Header("Zooming")]
     public float maxZoom = 5f;
     //public float rotSpeed = 10f;
     public float scrollSpeed = 3f;
@@ -17,6 +24,7 @@ public class CameraController : MonoBehaviour
     private float initialSize;
 
     private Camera camera;
+
 
 // Start is called before the first frame update
     void Start()
@@ -29,13 +37,35 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Get input
+        //Get movement change input
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
+        //Screen panning using mouse
+        if (Input.mousePosition.x >= Screen.width - Screen.width * panEdgeSize)
+        {
+            x += Time.deltaTime * panSpeed;
+        }
+
+        if (Input.mousePosition.x <= 0 + Screen.width * panEdgeSize)
+        {
+            x -= Time.deltaTime * panSpeed;
+        }
+
+        if (Input.mousePosition.y >= Screen.height - Screen.height * panEdgeSize)
+        {
+            y += Time.deltaTime * panSpeed;
+        }
+
+        if (Input.mousePosition.y <= 0 + Screen.height * panEdgeSize)
+        {
+            y -= Time.deltaTime * panSpeed;
+        }
+
+
+        //Stop movement based on bounds
         Vector3 currentPos = gameObject.transform.position;
         
-        //Stop movement based on bounds
         if((currentPos.x < leftBound && x < 0) || (currentPos.x > rightBound && x > 0))
         {
             x = 0f;
@@ -46,7 +76,9 @@ public class CameraController : MonoBehaviour
         }
 
         //Move camera
-        gameObject.transform.Translate(new Vector3(x, y, 0f));
+        gameObject.transform.Translate(new Vector3(x, y, 0f) * panSpeed * Time.deltaTime);
+
+
 
         //Get scroll
         zoom -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
@@ -56,5 +88,6 @@ public class CameraController : MonoBehaviour
         
         //Apply zoom
         camera.orthographicSize = zoom;
+        
     }
 }
