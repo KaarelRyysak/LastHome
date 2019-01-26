c3bd0c865af72a8763d0fd9175ef52b7236b456e
KaarelRyysak <kaarelgames@ryysak.com>
2019-01-26 10:41:19 +0200

    connect room\

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool open;
+    public Sprite openSprite;
+    public Sprite closedSprite;
+    private SpriteRenderer spriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+
+        //Inactive on startup
+        open = false;
+        spriteRenderer.sprite = closedSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //Toggle active on click
+        open = !open;
+
+        //Change sprite
+        if (open)
+        {
+            spriteRenderer.sprite = openSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = closedSprite;
+        }
+    }
+}
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,8 +1,28 @@
-﻿public class Room {
+﻿using System.Collections.Generic;
+
+public class Room {
 	public int y, x;
+    private List<Room> connected;
 
 	public Room(int y, int x) {
 		this.y = y;
 		this.x = x;
 	}
+
+    public List<Room> GetConnected()
+    {
+        return connected;
+    }
+
+    public void Connect(Room room)
+    {
+        connected.Add(room);
+        List<Room> otherConnected = room.GetConnected();
+        
+        //If the other room doesn't have this room, add it there too
+        if (!otherConnected.Contains(this))
+        {
+            room.Connect(this);
+        }
+    }
 }
