f5d08a9408b21e2b3ffc9998361d4379ee766834
TornOne <ott.adermann@gmail.com>
2019-01-27 12:56:10 +0200

    Null reference fix

--- Assets/Scripts/House/Room.cs
+++ Assets/Scripts/House/Room.cs
@@ -8,6 +8,10 @@ public class Room : MonoBehaviour {
 	public List<BaseTrap> traps = new List<BaseTrap>();
 	public HashSet<Human> humans = new HashSet<Human>();
 
+	void Awake() {
+		humans = new HashSet<Human>();
+	}
+
 	public Room(int y, int x) {
 		this.y = y;
 		this.x = x;
