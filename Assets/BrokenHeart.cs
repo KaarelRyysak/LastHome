c4d7965101e58f1518beea68fb7280b691bb9171
Mihkelt <mihkeltrei@gmail.com>
2019-01-27 15:48:37 +0200

    Heart

---
+++ Assets/BrokenHeart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenHeart : MonoBehaviour
+{
+    private float lifeTime = 1000f;
+    float speed = 0.25f;
+    void Update()
+    {
+        speed *= 1 + Time.deltaTime;
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+            Destroy(gameObject);
+    }
+}
