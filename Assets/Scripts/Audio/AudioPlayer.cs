edd9ae9ae8637bac49e29fc8386151f82df065c6
KaarelRyysak <kaarelgames@ryysak.com>
2019-01-27 09:47:24 +0200

    audio and camera

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+    public AudioGroup deathGroup;
+
+    public static AudioPlayer instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/Audio/AudioSourcePool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourcePool : MonoBehaviour
+{
+    private List<AudioSource> audioSources;
+
+
+
+    private void Awake()
+    {
+        audioSources = new List<AudioSource>();
+    }
+
+    public AudioSource GetSource()
+    {
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            if (!audioSources[i].isPlaying)
+            {
+                return audioSources[i];
+            }
+        }
+
+        GameObject newObject = new GameObject("AudioSource");
+        AudioSource audioSource = newObject.AddComponent<AudioSource>();
+        audioSources.Add(audioSource);
+
+        return audioSource;
+
+    }
+}
